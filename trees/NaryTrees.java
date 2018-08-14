cf433004cbc9cbaca9ef178ee94568e379ac4a05
ja153903 <jaimeabbariao@gmail.com>
2018-08-14 00:05:36 -0400

    Implemented level order traversal for nary tree

--- trees/NaryTrees.java
+++ trees/NaryTrees.java
@@ -1,8 +1,28 @@
-
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ArrayList;
 
 public class NaryTrees {
     public int val;
-    public List<NaryTrees> children;
+    public List<TreeNode> children;
+
+}
+
+class NaryTraversal {
+    // Probably the most efficient way to iterate through an nary tree
+    // since we can get screwed recursively
+    public void levelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            TreeNode cur = q.poll();
+            System.out.println(cur.val);
+            for (TreeNode child : cur.children) {
+                q.offer(child);
+            }
+        }
+    }
 }
